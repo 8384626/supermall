871082b687e613354aa708f18467fe6f9ffb0370
last code <ilastcode@163.com>
2020-12-16 22:23:17 +0800

    day03

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,9 +1,52 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <template #center>
+        <span>购物街</span>
+      </template>
+    </nav-bar>
+    <home-swiper :banners="banners" />
+    <recommend-view :recommends="recommends" />
+  </div>
 </template>
 
 <script>
-export default {};
+import NavBar from "components/common/navbar/NavBar";
+import HomeSwiper from "./childComps/HomeSwiper";
+import RecommendView from "./childComps/RecommendView";
+
+import { getHomeMultidata } from "network/home";
+export default {
+  name: "Home",
+  components: {
+    NavBar,
+    HomeSwiper,
+    RecommendView
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: []
+    };
+  },
+  created() {
+    // 1.请求多个数据
+    getHomeMultidata().then(res => {
+      if (res) {
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      }
+    });
+  }
+};
 </script>
 
-<style></style>
+<style scoped>
+.home-nav {
+  background-color: var(--color-tint);
+  color: aliceblue;
+}
+img {
+  width: 100%;
+}
+</style>
