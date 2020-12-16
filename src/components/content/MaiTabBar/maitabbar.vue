8b6a85a00d9694c2bce0b4f091eb8b3cddd63874
last code <ilastcode@163.com>
2020-12-16 13:12:39 +0800

    day02

---
+++ src/components/content/MaiTabBar/maitabbar.vue
@@ -0,0 +1,68 @@
+<template>
+  <tabbar>
+    <!-- 首页-->
+    <tabbaritem path="/home" activeColor="blue">
+      <template #item-icon>
+        <img src="~assets/img/tabbar/home.svg" alt="" />
+      </template>
+      <template #item-icon-active>
+        <img src="~assets/img/tabbar/home_active.svg" />
+      </template>
+      <template #item-text>
+        <div>首页</div>
+      </template>
+    </tabbaritem>
+
+    <!-- 分类 -->
+    <tabbaritem path="/category">
+      <template #item-icon>
+        <img src="~assets/img/tabbar/category.svg" alt="" />
+      </template>
+      <template #item-icon-active>
+        <img src="~assets/img/tabbar/category_active.svg" alt="" />
+      </template>
+      <template #item-text>
+        <div>分类</div>
+      </template>
+    </tabbaritem>
+
+    <!-- 购物车 -->
+    <tabbaritem path="/cart">
+      <template #item-icon>
+        <img src="~assets/img/tabbar/shopcart.svg" alt="" />
+      </template>
+      <template #item-icon-active>
+        <img src="~assets/img/tabbar/shopcart_active.svg" alt="" />
+      </template>
+      <template #item-text>
+        <div>购物车</div>
+      </template>
+    </tabbaritem>
+
+    <!-- 我的 -->
+    <tabbaritem path="/profile">
+      <template #item-icon>
+        <img src="~assets/img/tabbar/profile.svg" alt="" />
+      </template>
+      <template #item-icon-active>
+        <img src="~assets/img/tabbar/profile_active.svg" alt="" />
+      </template>
+      <template #item-text>
+        <div>我的</div>
+      </template>
+    </tabbaritem>
+  </tabbar>
+</template>
+
+<script>
+import tabbar from "components/common/tabbar/tabbar";
+import tabbaritem from "components/common/tabbar/tabbaritem";
+export default {
+  components: {
+    tabbar,
+    tabbaritem
+  }
+};
+</script>
+
+<style></style>
---
+++ src/router/index.js
@@ -0,0 +1,40 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+
+
+const routes = [{
+  path: '',
+  redirect: '/home'
+}, {
+  path: '/home',
+  name: 'Home',
+  component: () =>
+    import('views/home/home.vue'),
+},
+{
+  path: '/category',
+  name: 'Category',
+  component: () =>
+    import('views/category/category.vue'),
+},
+{
+  path: '/cart',
+  name: 'Cart',
+  component: () =>
+    import('views/cart/cart.vue'),
+},
+{
+  path: '/profile',
+  name: 'Profile',
+  component: () =>
+    import('views/profile/profile.vue'),
+},
+
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
